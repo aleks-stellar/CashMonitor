b6cf92c0d2b24cd5043c194ed866fb05a27dd677
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-07-31 23:58:25 +0300

    add PATH_TO_USER_SETTINGS

--- config/paths.py
+++ config/paths.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 PATH_TO_EXCEL_FILE = Path(Path(__file__).parent.parent, "data", "operations.xlsx")
 INVALID_PATH_TO_EXCEL_FILE = Path("operations.xlsx")
+PATH_TO_USER_SETTINGS = Path(Path(__file__).parent, "user_settings.json")
