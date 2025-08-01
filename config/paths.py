1c851f7e67c8cad66095807650a866afb4c15a92
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-08-01 22:31:28 +0300

    add paths

--- config/paths.py
+++ config/paths.py
@@ -3,3 +3,5 @@ from pathlib import Path
 PATH_TO_EXCEL_FILE = Path(Path(__file__).parent.parent, "data", "operations.xlsx")
 INVALID_PATH_TO_EXCEL_FILE = Path("operations.xlsx")
 PATH_TO_USER_SETTINGS = Path(Path(__file__).parent, "user_settings.json")
+PATH_TO_LOG_FILE = Path(Path(__file__).parent.parent, "logs", "utils.log")
+PATH_TO_LOG_DIR = Path(Path(__file__).parent.parent, "logs")
